6e7899d52cd106aafa4d9d585505a2b099a8f697
joao-paulo-f-pires <joao.pires@iscte.pt>
2016-03-04 19:57:23 +0000

    [Actividade 6 - Submeter Sugestão] Added empty actividade and added/updated associated files.

---
+++ PinPointHint/app/src/main/java/pt/iscte/daam/pinpointhint/SubmissionActivity.java
@@ -0,0 +1,19 @@
+package pt.iscte.daam.pinpointhint;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SubmissionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submission);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
