3c080b321bfccb6eefdeea793c9083917f1a27a8
rodas <joaorodrigocruz@gmail.com>
2016-05-14 17:08:04 +0100

    Added profile editing functionalities

--- PinPointHint/app/src/main/java/pt/iscte/daam/pinpointhint/UserDetailsActivity.java
+++ PinPointHint/app/src/main/java/pt/iscte/daam/pinpointhint/UserDetailsActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -21,6 +22,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import pt.iscte.daam.pinpointhint.LoginActivity;
 public class UserDetailsActivity extends AppCompatActivity {
@@ -34,6 +37,7 @@ public class UserDetailsActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
+        Button btActualizarPerfil = (Button) findViewById(R.id.btActualizarPerfil);
         emailLayout = (TextView) findViewById(R.id.tvUserEmail);
         nomeLayout = (TextView) findViewById(R.id.tvUserNome);
         etMudarNome = (EditText) findViewById(R.id.etMudarNome);
@@ -41,13 +45,94 @@ public class UserDetailsActivity extends AppCompatActivity {
 
 
         SharedPreferences sharedPreferences = getSharedPreferences("user_data",MODE_PRIVATE);
-        String email = sharedPreferences.getString("username","");
-        String password = sharedPreferences.getString("password","");
+        final String email = sharedPreferences.getString("username","");
+        //String password = sharedPreferences.getString("password","");
         String nome = sharedPreferences.getString("nome","");
 
         emailLayout.setText(email);
         nomeLayout.setText(nome);
 
+
+        btActualizarPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String nome = etMudarNome.getText().toString();
+                final String password = etMudarPw.getText().toString();
+
+                if(!isValidNome(nome)&&!isValidPassword(password)) {
+                    etMudarNome.setError("Insira um nome possível");
+                    etMudarPw.setError("Insira uma pw com 7 caracteres pelo menos");
+                    etMudarNome.requestFocus();
+                    etMudarPw.requestFocus();
+                }
+                else if(isValidNome(nome) && !isValidPassword(password)){
+                    Response.Listener<String> responseListener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");
+                                String nome = jsonResponse.getString("nome");
+                                if (success) {
+                                    SharedPreferences sharedPreferences = getSharedPreferences("user_data", MODE_PRIVATE);
+                                    sharedPreferences.edit().putString("nome", nome).apply();
+                                    Intent intent = getIntent();
+                                    finish();
+                                    startActivity(intent);
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+
+                    UserDetailsRequest userDetailsRequest = new UserDetailsRequest(email, nome, "", responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(UserDetailsActivity.this);
+                    queue.add(userDetailsRequest);
+                    }
+                 else if(isValidPassword(password) && !isValidNome(nome)){
+                    Response.Listener<String> responseListener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");
+                                if (success) {
+
+                                    Intent intent = getIntent();
+                                    finish();
+                                    startActivity(intent);
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+
+                    UserDetailsRequest userDetailsRequest = new UserDetailsRequest(email, "", password, responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(UserDetailsActivity.this);
+                    queue.add(userDetailsRequest);
+                }
+            }
+
+
+        });
+
+
+    }
+    private boolean isValidNome(String nome){
+        if(nome.length()>4 && !nome.matches(".*\\d.*")){
+            return true;
+        }
+        return false;
+
+
+    }
+    private boolean isValidPassword(String password){
+        if (password != null && password.length() > 6) {
+            return true;
+        }
+        return false;
     }
 
 }
---
+++ PinPointHint/app/src/main/java/pt/iscte/daam/pinpointhint/UserDetailsRequest.java
@@ -0,0 +1,31 @@
+package pt.iscte.daam.pinpointhint;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Cruz on 14/05/2016.
+ */
+public class UserDetailsRequest extends StringRequest{
+    private static final String USER_REQUEST_URL = "http://daam.coolpage.biz/userUpdate.php";
+    private Map<String,String> params;
+
+    public UserDetailsRequest(String email, String nome, String password, Response.Listener<String> listener){
+        super(Request.Method.POST,USER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("email",email);
+        params.put("nome", nome);
+        params.put("password", password);
+
+    }
+    @Override
+    public Map<String,String> getParams(){
+        return params;
+    }
+}
+
+
