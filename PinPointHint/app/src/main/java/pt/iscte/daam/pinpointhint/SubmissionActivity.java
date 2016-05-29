7da6267455a88eabf138dda2b02fe6c6d2f18e9e
rodas <joaorodrigocruz@gmail.com>
2016-05-29 16:38:15 +0100

    Submission commit

--- PinPointHint/app/src/main/java/pt/iscte/daam/pinpointhint/SubmissionActivity.java
+++ PinPointHint/app/src/main/java/pt/iscte/daam/pinpointhint/SubmissionActivity.java
@@ -4,8 +4,15 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -14,12 +21,21 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import pt.iscte.daam.pinpointhint.common.ActivityUtils;
 import pt.iscte.daam.pinpointhint.model.PinType;
@@ -35,6 +51,13 @@ public class SubmissionActivity extends AppCompatActivity implements View.OnClic
     private ImageView ivSub;
     private Button btSubNext;
     private Spinner spinnerSubType;
+    //newcode
+    private String encoded_string, image_name;
+    private Bitmap bitmap;
+    private File file;
+    private Uri file_uri;
+
+
 
     private Double lat;
     private final static String mLogTag = "pin point log";
@@ -80,14 +103,90 @@ public class SubmissionActivity extends AppCompatActivity implements View.OnClic
      */
     public void photo(View v) {
         //TODO
+
+
         //Aceder a função de foto do sistema operativo e guardar a imagem em memória
         ivSub.setVisibility(View.VISIBLE);
         btSubNext.setVisibility(View.VISIBLE);
 
-        Toast toast = Toast.makeText(getApplicationContext(),
-                R.string.photoLoaded, Toast.LENGTH_SHORT);
-        toast.show();
+        //Toast toast = Toast.makeText(getApplicationContext(),
+        //        R.string.photoLoaded, Toast.LENGTH_SHORT);
+        //toast.show();
+
+        //newcode
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        getFileUri();
+        i.putExtra(MediaStore.EXTRA_OUTPUT, file_uri);
+        startActivityForResult(i, 10);
+
+
     }
+
+    //newcode
+    private void getFileUri() {
+        String lat1 = String.valueOf(lat).substring(0,10);
+        image_name="" + lat1 + ".jpg";
+        // image_name.concat(String.valueOf(lat));
+        file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                + File.separator + image_name
+        );
+
+        file_uri = Uri.fromFile(file);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 10 && resultCode == RESULT_OK) {
+            new Encode_image().execute();
+        }
+    }
+    private class Encode_image extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            bitmap = BitmapFactory.decodeFile(file_uri.getPath());
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+
+            byte[] array = stream.toByteArray();
+            encoded_string = Base64.encodeToString(array, 0);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            makeRequest();
+        }
+    }
+
+    private void makeRequest() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.POST, "http://daam.coolpage.biz/connection.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> map = new HashMap<>();
+                map.put("encoded_string",encoded_string);
+                map.put("image_name",image_name);
+
+                return map;
+            }
+        };
+        requestQueue.add(request);
+    }
+
+
+
 
     /**
      * This method is used to handle the event to go to the next activity.
@@ -137,6 +236,10 @@ public class SubmissionActivity extends AppCompatActivity implements View.OnClic
         IncrementPinRequest incrementRequest = new IncrementPinRequest(email, responseListener);
         RequestQueue queue = Volley.newRequestQueue(SubmissionActivity.this);
         queue.add(incrementRequest);
+
+        //newcode
+
+
 
         finish();
 
